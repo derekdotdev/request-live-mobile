b205aaf681e2398f22899519e40df485e60e051a
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-04-15 17:11:50 -0400

    Fixed podfile error caused by ARM processor and added pre-compiled version of FirebaseFirestore code to podfile for improved build times.

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../widgets/auth/auth_form.dart';
+
+class AuthScreen extends StatefulWidget {
+  static const routeName = '/auth';
+
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  void _submitAuthForm(
+      String email, String password, String username, bool isLogin) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(
+        _submitAuthForm,
+      ),
+    );
+  }
+}
---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  final void Function(
+      String email, String password, String userName, bool isLogin) submitFn;
+
+  AuthForm(this.submitFn);
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    // Run validation on TextFormFields
+    final isValid = _formKey.currentState!.validate();
+
+    // Close the soft keyboard
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+      widget.submitFn(_userEmail, _userPassword, _userName, _isLogin);
+    }
+
+    // Send auth request with user data
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                // Only take as much size as necessary
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Email
+                  TextFormField(
+                    key: const ValueKey('email'),
+                    validator: (value) {
+                      if (value!.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: 'Email address',
+                    ),
+                    onSaved: (value) {
+                      _userEmail = value!;
+                    },
+                  ),
+                  // Username
+                  if (!_isLogin)
+                    TextFormField(
+                      key: const ValueKey('username'),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters.';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(labelText: 'Username'),
+                      onSaved: (value) {
+                        _userName = value!;
+                      },
+                    ),
+                  // Password
+                  TextFormField(
+                    key: const ValueKey('password'),
+                    validator: (value) {
+                      if (value!.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    onSaved: (value) {
+                      _userPassword = value!;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  TextButton(
+                    child: Text(_isLogin ? 'Log In' : 'Sign Up'),
+                    onPressed: _trySubmit,
+                  ),
+                  TextButton(
+                    child: Text(_isLogin
+                        ? 'Create new account'
+                        : 'I already have an account'),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
